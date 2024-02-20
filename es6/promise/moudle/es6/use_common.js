2ed7a2731f78fc1c7aae2ad8fba6453f600203fa
银小海 <643013242@QQ.COM>
2024-02-20 13:42:17 +0800

    第一种js模块化编程导入

---
+++ es6/promise/moudle/es6/use_common.js
@@ -0,0 +1,13 @@
+//导入
+/**
+ * 1. 我可以{} 来接收导出的数据
+ * 2. 可以全部接收，也可以选择的接收
+ * 3. 细节： 这时要求导入的名称和导出的名称一致
+ */
+import {sub,sum,name} from "./common";
+import {monster} from "./common";
+
+//使用
+console.log(sumx(10, 90));
+console.log(name);
+console.log(monster.hi())
