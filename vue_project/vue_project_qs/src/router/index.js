231673129023480001aa140519318081b1a61c59
银小海 <643013242@QQ.COM>
2024-03-22 19:33:57 +0800

    引入ElementUI插件

--- vue_project/vue_project_qs/src/router/index.js
+++ vue_project/vue_project_qs/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+//@表示src目录
+import helloyinhai from '@/components/helloyinhai'
+import yinhai from '@/components/yinhai'
 
 Vue.use(Router)
 
@@ -10,6 +13,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/hello',
+      name: 'helloyinhai',
+      component: helloyinhai
+    },
+    {
+      path: '/yinhai',
+      name: 'yinhai',
+      component: yinhai
     }
   ]
 })
