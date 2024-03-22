d237af168bc0faa662bd3a525bea06182c2633e5
银小海 <643013242@QQ.COM>
2024-03-22 19:33:27 +0800

    使用路由实现网站部署

---
+++ vue_project/vue_project_qs/src/components/helloyinhai.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>
+      {{ mes }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "helloyinhai",
+  data(){
+    return{
+      mes:"hello yinhai"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
